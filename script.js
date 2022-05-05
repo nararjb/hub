2fcd1b4f6b23129c88c0dc044bbf98e9a6259679
nararjb <104914263+nararjb@users.noreply.github.com>
2022-05-06 00:42:01 +0300

    Script

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const navMenu = document.querySelector(".nav-menu");
 const navBar = document.querySelector(".navbar");
 const navSocial = document.querySelector(".nav-social");
 const navSocialCol = document.querySelector(".nav-social-col");
+const navOverlay = document.querySelector(".nav-overlay");
+
 
 menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("x");
@@ -10,4 +12,14 @@ menuIcon.addEventListener("click", () => {
     navBar.classList.toggle("x");
     navSocial.classList.toggle("x");
     navSocialCol.classList.toggle("x");
+    navOverlay.classList.toggle("x");
+})
+
+navOverlay.addEventListener("click", () => {
+    menuIcon.classList.toggle("x");
+    navMenu.classList.toggle("x");
+    navBar.classList.toggle("x");
+    navSocial.classList.toggle("x");
+    navSocialCol.classList.toggle("x");
+    navOverlay.classList.toggle("x");
 })
